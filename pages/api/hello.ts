45d4a83b5db54a9e64527bdedcbb94ad25461120
Denis Carriere <carriere.denis@gmail.com>
2022-06-02 15:27:35 -0400

    add redis

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || "",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
+})
 
 type Data = {
-  name: string
+  name: string,
+  data: number | null,
 }
 
-export default function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler( req: NextApiRequest, res: NextApiResponse<Data> ) {
+  const previous = await redis.get<number>('count') || 0;
+  const current = previous + 1;
+  await redis.set<number>('count', current );
+  res.status(200).json({ name: 'John Doe', data: current })
 }
