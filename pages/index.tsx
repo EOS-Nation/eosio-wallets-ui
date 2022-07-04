8e43056ddfd5db6ba835bf7e36125d68ba7c2e6e
YaroShkvorets <shkvorets@gmail.com>
2022-07-04 10:13:00 -0400

    improve error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,12 @@
 import type { NextPage } from 'next'
-import { useState } from "react"
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Login, Logout } from '../components/Wallet'
-import { Quantity } from '../components/Quantity'
-import { Transaction } from "../components/Transaction";
-import { Highlight } from "../components/Highlight";
+import { Main } from "./main"
+import { SnackbarProvider } from 'notistack'
 
 const Home: NextPage = () => {
-  const [ actor, setActor ] = useState<string>("");
-  const [ transaction_id, setTransactionId ] = useState<string>();
-  const [ protocol, setProtocol ] = useState<"scatter"|"anchor">();
-  const [ quantity, setQuantity ] = useState<string>("");
-  const [ cosign, setCosign ] = useState<boolean>(false);
 
-  const wallets = !actor ? (
-    <div className={styles.grid}>
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/anchor.svg" name="Anchor" protocol={"anchor"} />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/wombat.png" name="Wombat" />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/tokenpocket.png" name="TP" />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/scatter.svg" name="Scatter" />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/imtoken.png" name="ImToken" />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/start.png" name="Start" />
-      <Login setProtocol={ setProtocol } setActor={ setActor } img="/metahub.png" name="Metahub" />
-    </div>
-  ) : '';
-
-  const quantities = actor ? (
-    <div className={styles.grid}>
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "1.0000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.5000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.2500 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.1000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.0500 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.0001 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } />
-    </div>
-  ) : '';
-
-  const logout = actor ? (
-    <div className={styles.grid}>
-      <Logout setProtocol={ setProtocol } setActor={ setActor } />
-    </div>
-  ) : '';
 
   return (
     <div className={styles.container}>
@@ -52,27 +16,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          EOS Wallets
-        </h1>
-        <p className={styles.description}>
-          { protocol ? "✅" : '1. Select' } Wallet <Highlight str={ protocol } /><br/>
-          { actor ? "✅" : '2. Login' } Account <Highlight str={ actor } /><br/>
-          { actor && quantity ? "✅" : '3. Select' } Quantity <Highlight str={ actor ? quantity : "" } /><br/>
-          <Transaction transaction_id={ transaction_id } /><br/><br/>
-          <label>
-            <input
-              type="checkbox"
-              checked={ cosign }
-              onChange={ () => setCosign(!cosign) }
-            /> Cosign
-          </label>
-        </p>
-        { wallets }
-        { quantities }
-        { logout }
-      </main>
+
+      <SnackbarProvider
+        maxSnack={5}
+        autoHideDuration={5000}
+      >
+        <Main/>
+      </SnackbarProvider>
 
       <footer className={styles.footer}>
         <a
---
+++ pages/main.tsx
@@ -0,0 +1,79 @@
+import type { NextPage } from 'next'
+import { useState } from "react"
+import styles from '../styles/Home.module.css'
+import { Login, Logout } from '../components/Wallet'
+import { Quantity } from '../components/Quantity'
+import { Transaction } from "../components/Transaction";
+import { Highlight } from "../components/Highlight";
+import { useSnackbar } from "notistack";
+
+export function Main() {
+  const [ actor, setActor ] = useState<string>("");
+  const [ transaction_id, setTransactionId ] = useState<string>();
+  const [ protocol, setProtocol ] = useState<"scatter"|"anchor">();
+  const [ quantity, setQuantity ] = useState<string>("");
+  const [ cosign, setCosign ] = useState<boolean>(false);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const wallets = !actor ? (
+    <div className={styles.grid}>
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/anchor.svg" name="Anchor" protocol={"anchor"} />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/wombat.png" name="Wombat" />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/tokenpocket.png" name="TP" />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/scatter.svg" name="Scatter" />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/imtoken.png" name="ImToken" />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/start.png" name="Start" />
+      <Login setProtocol={ setProtocol } setActor={ setActor } img="/metahub.png" name="Metahub" />
+    </div>
+  ) : '';
+
+  const FlashMessage = (message: string, type: 'error'|'success'|'warning'|'info') => {
+    enqueueSnackbar(message, { variant: type });
+  }
+
+  const quantities = actor ? (
+    <div className={styles.grid}>
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "1.0000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage}/>
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.5000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage} />
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.2500 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage} />
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.1000 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage} />
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.0500 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage} />
+      <Quantity setQuantity={ setQuantity } setTransactionId={ setTransactionId } quantity={ "0.0001 EOS" } actor={ actor } protocol={ protocol } cosign={ cosign } flash={FlashMessage} />
+    </div>
+  ) : '';
+
+  const logout = actor ? (
+    <div className={styles.grid}>
+      <Logout setProtocol={ setProtocol } setActor={ setActor } />
+    </div>
+  ) : '';
+
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          EOS Wallets
+        </h1>
+        <p className={styles.description}>
+          { protocol ? "✅" : '1. Select' } Wallet <Highlight str={ protocol } /><br/>
+          { actor ? "✅" : '2. Login' } Account <Highlight str={ actor } /><br/>
+          { actor && quantity ? "✅" : '3. Select' } Quantity <Highlight str={ actor ? quantity : "" } /><br/>
+          <Transaction transaction_id={ transaction_id } /><br/><br/>
+          <label>
+            <input
+              type="checkbox"
+              checked={ cosign }
+              onChange={ () => setCosign(!cosign) }
+            /> Cosign
+          </label>
+        </p>
+        { wallets }
+        { quantities }
+        { logout }
+      </main>
+    </div>
+  )
+}
+
+export default Main
